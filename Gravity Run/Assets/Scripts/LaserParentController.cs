e99f71b6f6fb9c33b6aa521293fe594f2c847b41
Alan <my_ant1@naver.com>
2024-09-15 20:53:03 -0400

    Laser Vibration sound added, beta ready

--- Gravity Run/Assets/Scripts/LaserParentController.cs
+++ Gravity Run/Assets/Scripts/LaserParentController.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserParentController : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    public AudioClip laserVibration;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = laserVibration;
+        audioSource.loop = true;
+        audioSource.volume = 0f;
+        audioSource.Play();
+
         transform.rotation = OctaCylinderContainerController.instance.transform.rotation;
     }
 
@@ -15,7 +26,12 @@ public class LaserParentController : MonoBehaviour
     {
         if (GameManager.instance.isGameOver) return;
 
-        if (transform.position.x < -5.0f)
+        if (transform.position.x < 55f && transform.position.x > -5f)
+            audioSource.volume = Math.Min(0.4f, 0.4f * (1f - (transform.position.x + 5f) / 60f));
+        else if (transform.position.x < -5f) 
+            audioSource.volume = Math.Max(0f, 0.4f * (25f + transform.position.x) / 20f);
+
+        if (transform.position.x < -25.0f)
             Destroy(gameObject);
     }
 
